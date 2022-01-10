65118dc46316182416072f53995382fae1efedb0
ankit-deshmukh <ankit.deshmukh@smallcase.com>
2022-01-10 22:45:44 +0530

    build: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:startup_namer/randomWords.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData(          // TODO 7: Add a theme for the app
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+      ),
+      home: RandomWords(),
+    );
+  }
+}
---
+++ lib/randomWords.dart
@@ -0,0 +1,129 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+//TODO 1: type stfl and the 2 StatefulWidget classes would appear with the provision to choose the class name
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[]; //Array/List of Word pairs
+  final _saved = <WordPair>{}; //Set of Word pairs
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  void _pushSaved() {
+    // TODO 5: push a route to Navigator (move from 1 page to another)
+    // TODO 6: Note: The back button on the App bar calls Navigator.pop()
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+                (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+
+    final alreadySaved = _saved.contains(pair);
+
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon( //TODO 2: Add an icon to ListTitle
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+      ),
+      onTap: () {   //TODO 3: change state when list title is tapped
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      // The itemBuilder callback is called once per suggested
+      // word pairing, and places each suggestion into a ListTile
+      // row. For even rows, the function adds a ListTile row for
+      // the word pairing. For odd rows, the function adds a
+      // Divider widget to visually separate the entries. Note that
+      // the divider may be difficult to see on smaller devices.
+      itemBuilder: (context, i) {
+        // Add a one-pixel-high divider widget before each row
+        // in the ListView.
+        if (i.isOdd) {
+          return const Divider();
+        }
+
+        // The syntax "i ~/ 2" divides i by 2 and returns an
+        // integer result.
+        // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+        // This calculates the actual number of word pairings
+        // in the ListView,minus the divider widgets.
+        final index = i ~/ 2;
+        // If you've reached the end of the available word
+        // pairings...
+        if (index >= _suggestions.length) {
+          // ...then generate 10 more and add them to the
+          // suggestions list.
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+        // TODO 4: Add action menu to AppBar
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: _pushSaved,
+            tooltip: 'Saved Suggestions',
+          ),
+        ],
+        // ... to here
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+}
